5fdb93e4c259f4b7fb2412c070812637e45670fd
Graham <gleachkr@gmail.com>
2024-05-21 16:15:36 -0400

    Fix template attribute

--- flake.nix
+++ flake.nix
@@ -3,21 +3,22 @@
 
   inputs.flake-utils.url  = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils}: flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
     let pkgs = nixpkgs.legacyPackages.${system};
     in {
 
-      devShell = pkgs.mkShell { 
+      devShell = with pkgs; mkShell { 
         buildInputs = [ 
-          pkgs.nodePackages.vscode-langservers-extracted
-          pkgs.nodePackages.typescript-language-server 
-          pkgs.nodePackages.browser-sync
+          nodePackages.vscode-langservers-extracted
+          nodePackages.typescript-language-server 
+          nodePackages.browser-sync
         ]; 
       };
 
+    }) // {
       templates.default = { 
         path = ./template; 
         description = "A pandoc-powered razzle presentation";
       };
-  });
+    };
 }
